d550dc6139eee6450e8cfa83c80f5c65a62fe630
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-20 12:35:45 +0100

    Add Step 3 code

--- IntroductionToCSharpCLI/Program.cs
+++ IntroductionToCSharpCLI/Program.cs
@@ -25,3 +25,15 @@ Console.WriteLine($"Hello {aFriend}");
 
 Console.WriteLine();
 #endregion
+
+#region step3
+Console.WriteLine("Step 3:");
+
+string firstFriend = "Maria";
+string secondFriend = "Sage";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}.");
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+Console.WriteLine();
+#endregion
