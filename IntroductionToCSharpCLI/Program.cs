ca6aabad7a0117a871ec4b3f169aa82ba6cbb009
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-20 12:36:01 +0100

    Add Step 4 code

--- IntroductionToCSharpCLI/Program.cs
+++ IntroductionToCSharpCLI/Program.cs
@@ -37,3 +37,29 @@ Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.")
 
 Console.WriteLine();
 #endregion
+
+#region step4
+Console.WriteLine("Step 4:");
+
+string greeting = "             Hello World!          ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
+
+Console.WriteLine();
+#endregion
