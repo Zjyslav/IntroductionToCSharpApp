9b7713f6f9eb877f38562c387bdafef2e90aae2b
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-20 12:34:50 +0100

    Add Step 1 code

--- IntroductionToCSharpCLI/Program.cs
+++ IntroductionToCSharpCLI/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Hello World - Introduction to C#
 // https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/hello-world
+
+Console.WriteLine("Hello World - Introduction to C# tutorial:");
+Console.WriteLine();
+
+#region step1
+Console.WriteLine("Step 1:");
+
+Console.WriteLine("Hello World!");
+
+Console.WriteLine();
+#endregion
